be23fbb804eee15eb29d66dc9273e15ac1950571
Nhat Vo <nhat_vo@datahouse.com>
2023-06-26 10:27:53 +0700

    'commit'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
-
+const APP_ID = "1388353131952099";
+const APP_SECRET = "c46a85e5c6e353e3d5f215e5e4aee00a";
 const App = () => {
   const [isLoggedin, setIsLoggedin] = useState(false);
 
   const onLoginClick = () => {
     window.FB.login(
-      function (response) {
-        console.log(response);
+      async function (response) {
+        const accessToken = response.authResponse.accessToken;
+        const longLivedUserTokenEndpoint = `https://graph.facebook.com/v17.0/oauth/access_token?grant_type=fb_exchange_token&client_id=${APP_ID}&client_secret=${APP_SECRET}&fb_exchange_token=${accessToken}`;
+        const { access_token: longLivedUserToken } = await fetch(
+          longLivedUserTokenEndpoint
+        ).then((res) => res.json());
+        const userIdEndpoint = `https://graph.facebook.com/v17.0/me?access_token=${longLivedUserToken}`;
+        const { id: userId } = await fetch(userIdEndpoint).then((res) =>
+          res.json()
+        );
+        const longLivedPageTokenEndpoint = `https://graph.facebook.com/v17.0/${userId}/accounts?access_token=${longLivedUserToken}`;
+        const { data } = await fetch(longLivedPageTokenEndpoint).then((res) =>
+          res.json()
+        );
+        console.log("final token", data?.[0].access_token);
       },
       {
         scope:
@@ -18,7 +32,7 @@ const App = () => {
   useEffect(() => {
     window.fbAsyncInit = () => {
       window.FB.init({
-        appId: "1388353131952099",
+        appId: APP_ID,
         autoLogAppEvents: true,
         xfbml: true,
         version: "v11.0",
